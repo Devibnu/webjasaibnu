16c4ff6e6566fee9225a30627ca66657879a40d5
Nanang Ibnu Qosim <devibnuq@gmail.com>
2026-08-15 07:48:40 +0700

    Set public HTML language to Indonesian

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="id">
 <head>
     @php
         $siteName = $siteSettings->company_legal_name ?? config('app.name', 'PT JASA IBNU DEVELOPMENT');
